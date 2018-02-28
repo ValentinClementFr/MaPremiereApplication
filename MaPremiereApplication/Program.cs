7c161f0dccc873b5c4f99f6d86077e2b088be5ae
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-02-28 11:17:50 +0100

    Premier TP

--- MaPremiereApplication/Program.cs
+++ MaPremiereApplication/Program.cs
@@ -10,7 +10,54 @@ namespace MaPremiereApplication
     {     
         static void Main(string[] args)
         {
-            Console.WriteLine(Environment.UserName);
+            string utilisateur = Environment.UserName;
+            int heureCourante = DateTime.Now.Hour;
+
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                if(heureCourante < 9)
+                {
+                    Console.WriteLine("Bon week-end " + utilisateur);
+                }
+                else if (heureCourante >= 9 && heureCourante <= 18)
+                {
+                    Console.WriteLine("Bonjour " + utilisateur);
+                }
+                else if (heureCourante > 18)
+                {
+                    Console.WriteLine("Bonsoir " + utilisateur);
+                }
+            }
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Tuesday || DateTime.Now.DayOfWeek == DayOfWeek.Wednesday || DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
+            {
+                if(heureCourante >= 9 && heureCourante <= 18)
+                {
+                    Console.WriteLine("Bonjour " + utilisateur);
+                }
+                else if(heureCourante < 9 || heureCourante > 18)
+                {
+                    Console.WriteLine("Bonsoir " + utilisateur);
+                }
+            }
+            else if(DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                if(heureCourante < 9)
+                {
+                    Console.WriteLine("Bonsoir " + utilisateur);
+                }
+                else if (heureCourante >= 9 && heureCourante <= 18)
+                {
+                    Console.WriteLine("Bonjour " + utilisateur);
+                }
+                else if(heureCourante > 18)
+                {
+                    Console.WriteLine("Bon week-end " + utilisateur);
+                }
+            }
+            else if(DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Bon week-end " + utilisateur); 
+            }
         }
     }
 }
