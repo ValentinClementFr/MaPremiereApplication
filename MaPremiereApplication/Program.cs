a9dbf81286207f2960e5924078ce0699fe3dbec1
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-02-28 09:55:21 +0100

    Push Maj Application

--- MaPremiereApplication/Program.cs
+++ MaPremiereApplication/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MaPremiereApplication
 {
     class Program
-    {
+    {     
         static void Main(string[] args)
         {
-            Console.WriteLine("Test GitHub");
+            Console.WriteLine(Environment.UserName);
         }
     }
 }
